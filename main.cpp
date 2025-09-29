08a3ceca382a8de5c3b8c419dfb4da5f865910f1
Potet <142875697+Kittel-Watne@users.noreply.github.com>
2025-09-29 09:21:27 +0200

    Sound Example in Main
    Need assets before i can make it work proper. Will probably never use it this way, but this could technically work.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include "MainWindow.h"
+#include "wavfilereader.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    ///Loading Sounds
+    /*
+    const std::string ASSET_PATH = NULL;
+    wave_t *exampleSound = new wave_t;
+    WavFileReader SoundFileReader;
+    SoundFileReader.loadWave(ASSET_PATH, exampleSound);
+    */
+
     QApplication a(argc, argv);
     MainWindow w;
     w.move(200, 100);
