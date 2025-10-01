d8c21895b8c3ed150b00c874d608a11c38f4972d
Edvard Andreassen <mitori0211@gmail.com>
2025-10-02 00:39:50 +0200

    updated values
    changed some values while testing

--- Test1.c
+++ Test1.c
@@ -2,14 +2,15 @@
 #include <math.h>
 #include <stdio.h>
 
-#define PI 3.14159265359
+//#define PI 3.14159265359
 
 float findAngle(Vector2 A, Vector2 B, Vector2 C){
     float AB = sqrt(((B.x - A.x)*(B.x - A.x)) + ((B.y - A.y)*(B.y - A.y)));
     float BC = sqrt(((C.x - B.x)*(C.x - B.x)) + ((C.y - B.y)*(C.y - B.y)));
-    float angleRad = BC / AB;
+    float angleRad = (BC / AB);
+    float angle = acos(angleRad);
     float angleDeg = angleRad * (180 / PI);
-    printf("angle is: %f\n", angleDeg);
+    printf("angle is: %f\n", angle);
     return angleDeg;
 }
 
@@ -28,9 +29,9 @@ int main(void)
     Vector2 ball2Pos = {(float)width/2, (float)height/4};
     Vector2 velocity2 = {0.0f, 0.0f};
     
-    Vector2 triPosA = {300.0f, 1200.0f};
+    Vector2 triPosA = {200.0f, 1200.0f};
     Vector2 triPosB = {800.0f, 1200.0f};
-    Vector2 triPosC = {800.0f, 700.0f};
+    Vector2 triPosC = {800.0f, 800.0f};
     Vector2 triPosD = {0.0f, 700.0f};
     Vector2 triPosE = {0.0f, 1200.0f};
     
@@ -55,8 +56,9 @@ int main(void)
                 DrawText("balls be balling", 190, 400, 20, RAYWHITE);
                 velocity.x += velocity.y;
                 velocity.y += -velocity.x/2;
+                velocity.y *= -0.45;
                 velocity.x *= -0.95;
-                velocity.y *= -0.5;
+                
             }
             
             ballPos.x += velocity.x;
@@ -68,7 +70,7 @@ int main(void)
             {
                 velocity.y *= -0.95;
             }
-            if(ballPos.x < 0 || ballPos.x > width)
+            if(/*ballPos.x < 0 || */ballPos.x > width)
             {
                 velocity.x *= -0.95;
             }
