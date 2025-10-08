80f25cc8af51c174ccb5e9a1918ef413f5844f1e
Ole E. Flaten <ole.flaten@inn.no>
2025-10-08 19:29:36 +0200

    More cleanup - added WavfileReader.

--- WavfileReader.h
+++ WavfileReader.h
@@ -1,10 +1,13 @@
-/*#ifndef SOUNDCOMPONENT_H
-#define SOUNDCOMPONENT_H
+//Based off Ole Flatens code from: https://github.com/oleflaten/3D-Sound-OpenAL
 
-#include <cstdint>
+#ifndef FILEHANDLER_H
+#define FILEHANDLER_H
+
+#include <cstdint>    //does not work well on mac
 #include <string>
 #include <cstdio>
 
+/// Structure to contain WAVE file data.
 struct wave_t
 {
     uint32_t size;              ///< Size of the WAVE file.
@@ -22,6 +25,7 @@ struct wave_t
     wave_t() : buffer(NULL) {}  ///< Structure Constructor. Initialises data buffer to NULL.
 };
 
+/// Class to handle loading of Microsoft signed 16-bit PCM WAVE audio files.
 class WavFileReader
 {
 public:
@@ -30,15 +34,14 @@ public:
         \param File path relative to execution directory.
         \param Pointer to a wave_t structure to contain the wave data.
     **/
-    /*static bool loadWave(std::string filePath, wave_t* wavePtr);
+    static bool loadWave(std::string filePath, wave_t* wavePtr);
 private:
     /// Error handling function.
     /**
         Always returns false, outputs argumen string to the command line.
         \param Error message to output.
-    **/"
-   /* static bool endOnError(std::string errmsg);
+    **/
+    static bool endOnError(std::string errmsg);
 };
 
-
-#endif // SOUNDCOMPONENT_H
+#endif
