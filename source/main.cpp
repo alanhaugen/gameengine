1883469bf231e9cf3ba227e3edfb4831c25fa3f7
Hans Alan Whitburn Haugen <alanhaugen@gmail.com>
2025-10-21 21:00:42 +0200

    Tower defense game start

--- source/main.cpp
+++ source/main.cpp
@@ -1,19 +1,11 @@
 #include "platforms/application.h"
-#include "demos/vikingscene.h"
-#include "demos/empty.h"
-#include "demos/pong.h"
-#include "demos/flappybird.h"
-#include "demos/breakout.h"
-#include "demos/rollingball.h"
 
 int main(int argumentQuantity, char *argumentArray[])
 {
     Application application(argumentQuantity, argumentArray, "Breakout", 1100, 700);
-    //application.AddScene(new Empty);
-    //application.AddScene(new VikingScene);
-    //application.AddScene(new RollingBall);
-    //application.AddScene(new Pong);
-    //application.AddScene(new FlappyBird);
-    application.AddScene(new Breakout);
+    application.AddScene(new MainMenu);
+    application.AddScene(new Game);
+    application.AddScene(new GameOver);
+    application.AddScene(new Victory);
     return application.Run();
 }
