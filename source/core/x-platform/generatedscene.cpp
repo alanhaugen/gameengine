1d9189ec842fcba3908e873443b72ff5ac511471
Alan Haugen <alanhaugen@gmail.com>
2025-11-16 06:54:23 +0100

    Changing gui

---
+++ source/core/x-platform/generatedscene.cpp
@@ -0,0 +1,17 @@
+#include "generatedscene.h"
+
+GeneratedScene::GeneratedScene()
+{
+}
+
+void GeneratedScene::Init()
+{
+}
+
+void GeneratedScene::Update()
+{
+}
+
+void GeneratedScene::Clean()
+{
+}
---
+++ source/core/x-platform/generatedscene.h
@@ -0,0 +1,15 @@
+#ifndef GENERATEDSCENE_H
+#define GENERATEDSCENE_H
+
+#include "core/x-platform/scene.h"
+
+class GeneratedScene : public Scene
+{
+public:
+    GeneratedScene();
+    void Init();
+    void Update();
+    void Clean();
+};
+
+#endif // GENERATEDSCENE_H
